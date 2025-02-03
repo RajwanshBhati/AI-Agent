6b97fc619df6c526520204ca497bb085a8aeb8c0
Rajwansh Bhati <rajwanshbhati@gmail.com>
2025-02-03 22:33:02 +0530

    MIDDEL

--- Backend/routes/project.routes.js
+++ Backend/routes/project.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
 import * as projectController from '../controllers/project.controller.js';
-import * as authMiddleWare from '../middleware/auth.middleware.js';
+import * as authMiddleWare from '../Middleware/auth.middleware.js';
 
 const router = Router();
 
